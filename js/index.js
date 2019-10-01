c76dec1b2d5209037e1d5fcd5af9c73be24c2080
Fuvanbuh <gerardfunes@telefonica.net>
2019-10-01 22:27:05 +0200

    Lab sin acabar

--- js/index.js
+++ js/index.js
@@ -3,16 +3,32 @@ function deleteItem(event){
 }
 
 function getPriceByProduct(itemNode){
+  let totalSuma = 0; 
+ let arrayPrecios = [...document.getElementsByClassName('precio')];
+ let cantproduct = [...document.getElementsByClassName("productname")];
 
-}
+ for (let i = 0; i < arrayPrecios.length; i++){
+  totalSuma = (parseFloat(cantproduct[i].value)* parseFloat(arrayPrecios[i]).innerText);
+ }
+ return totalSuma;
+ }
+  
+
+
 
 function updatePriceByProduct(productPrice, index){
 
 }
 
 function getTotalPrice() {
+let total = [...document.getElementsByClassName('preciototal')];
+for (let i = 0; i < total.length; i++){
+  parseFloat((total[i] += total[i]).value).innerText;
 
 }
+  console.log(total)
+}
+getTotalPrice()
 
 function createQuantityInput(){
 
